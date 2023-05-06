6e4fa46f4cf6dba1c02e025ded8a4d5ef53d0142
SnehalGidde <123617515+SnehalGidde@users.noreply.github.com>
2023-05-06 14:15:18 +0530

    creat additional info
    creat additional info

---
+++ 03_typeOf.js
@@ -0,0 +1,17 @@
+var bankname;
+console.log("value and type of bank name before assign:",bankname, typeof bankname);
+bankname = 'SBI Bank';
+console.log(" value and type of bank name after initialization:", bankname, typeof bankname);
+console.log("\n");
+var marks = '90%';
+console.log("value and type of marks:", marks, typeof marks);
+console.log("\n");
+var isWorking;
+console.log("value and type of bank name before initialization:",isWorking, typeof isWorking);
+isWorking = true;
+console.log(" value and type of bank name after initialization:", isWorking, typeof isWorking);
+console.log("\n");
+var totalCount = "Hundred and Seven";
+console.log("value and datatype of totalCount is -> :",totalCount,typeof totalCount );
+ totalCount = 107;
+console.log("value and datatype of totalCount is -> :",totalCount,typeof totalCount);
