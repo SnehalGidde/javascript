aced98b06f970c93f03a738f810e3f412e8c0f25
SnehalGidde <123617515+SnehalGidde@users.noreply.github.com>
2023-05-06 13:26:18 +0530

    basic variable
    basic variable

---
+++ 02_variableBasicsAssig.js
@@ -0,0 +1,25 @@
+console.log("**Data Type Number**");
+var phoneNo = 9370195534;
+console.log("value of variable-> phoneno:", phoneNo);
+console.log('type of variable -> phoneno is:', typeof phoneNo);
+console.log("\n");
+
+console.log("**Data Type String**");
+var profession = "Technical Assistant";
+console.log("value of variable-> profession:", profession);
+console.log('type of variable -> profession is:', typeof profession);
+console.log("\n");
+
+console.log("**Data Type Boolean**");
+var isMarried = true;
+console.log("value of variable-> isMarried:", isMarried);
+console.log('type of variable -> isMarried is:', typeof isMarried);
+console.log("\n");
+console.log("***Difference between undefined ans null**");
+console.log("Undefined means variable is declared but it's value is not assigned and \n null means variable is declared but it's value is assigned as null value\n so we get type of undefined as undefined and type of null as null ");
+console.log("\n");
+var collegeName;
+console.log("collegeNmae variable declared but not assigned then value of collegeName variable is:");
+console.log(collegeName);
+var code = null;
+console.log(code);
