36dafe853f83d6008c1817525d1a4f5a0e824655
SnehalGidde <123617515+SnehalGidde@users.noreply.github.com>
2023-05-06 07:36:56 +0530

    added some information
    added some information

---
+++ hello.js
@@ -0,0 +1,34 @@
+
+
+
+
+console.log("Snehal gidde");
+
+var firstName="Snehal";
+console.log(firstName);
+
+var age;
+age="24";
+console.log(age);
+
+var holder;
+console.log(holder);
+holder="My spec";
+console.log(holder);
+
+console.log("Gajanan Kharat..");
+
+var firstName = "Gajanan";
+console.log(firstName);
+
+var age; // Variable Declaration 
+age = "22";  // Variable Initialization
+console.log(age);
+
+var holder; // Variable Declaration
+console.log(holder); // undefined
+holder = "My Spec"; // Variable Initialization
+console.log(holder);
+
+holder = "Sachin Spec"; // Variable update
+console.log(holder); //
---
+++ variable_assign.js
@@ -0,0 +1,30 @@
+var holder; // Variable Declaration
+console.log(holder);
+
+var fullName=("Snehal Ganesh Gidde");
+console.log(fullName);
+
+var firstName=("Snehal");
+console.log(firstName);
+
+
+var lastName=("Gidde");
+console.log(lastName);
+
+var myLove="I love only JS";
+console.log(myLove);
+
+var age="27";
+console.log(age);
+
+var address="Yogeshwari residency marunji, pune"
+console.log(address);
+
+var pincode="411002";
+console.log(pincode);
+
+var gender="Female";
+console.log(gender);
+
+var isMarried="Yes";
+console.log(isMarried);
